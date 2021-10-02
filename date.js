102f0c3045f5ade4947cf28b4e0f20c4bdda0582
Riya Kumari Jha <riya.jha2305@gmail.com>
2021-10-03 04:21:11 +0530

    Initial commit

---
+++ date.js
@@ -0,0 +1,28 @@
+// //jshint esversion:6
+
+// //module.exports.getDate = getDate; // don't add parenthese now because we don't want our function to run now add parenthese on app.js page on date()
+
+// //module.exports is same as just exports so we can have a shortcut
+// exports.getDate = function(){
+//     const today = new Date();
+    
+//     const options = {
+//         weekday: "long",
+//         day: "numeric",
+//         month: "long",
+//     };
+
+//     return today.toLocaleDateString("en-US", options);
+// }
+
+// exports.getDay = function(){
+//     const today = new Date();
+    
+//     const options = {
+//         weekday: "long",
+//     };
+
+//     return today.toLocaleDateString("en-US", options);
+// }
+
+// console.log(module.exports);
